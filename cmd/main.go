33728054f462a431dac8bb6630ee56fac3b6bc71
Bruno Silva Freire <bruno@stageglass.com>
2024-08-03 18:42:01 -0300

    Adding load balancer management

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
 	"log"
 	"os"
 	"os/signal"
@@ -13,31 +14,66 @@ import (
 
 func main() {
 	logger := log.Default()
+	level := klog.Level(6)
+	err := level.Set("6")
+	if err != nil {
+		logger.Printf("Failed to set klog level: %s", err.Error())
+		return
+	}
 	config, success := loadConfig(logger)
 	if !success {
 		logger.Printf("Failed to load kubernetes config")
 		return
 	}
 
-	logger.Printf("Kubernetes config: %v", config.String())
+	endpoint, found := os.LookupEnv("ROUTER_OVERRIDE_KUBERNETES_ENDPOINT")
+
+	if found {
+		logger.Printf("Overriding Kubernetes endpoint to %s", endpoint)
+		config.Host = endpoint
+	}
+
+	logger.Printf("Final kubernetes config: %v", config.String())
 
 	kube := kubernetes.NewForConfigOrDie(config)
+
 	URL, found := os.LookupEnv("ROUTER_URL")
 	if !found {
 		logger.Printf("ROUTER_URL not found")
 		return
 	}
-	Username, found := os.LookupEnv("ROUTER_USERNAME")
+
+	username, found := os.LookupEnv("ROUTER_USERNAME")
 	if !found {
 		logger.Printf("ROUTER_USERNAME not found")
 		return
 	}
-	Password, found := os.LookupEnv("ROUTER_PASSWORD")
+
+	password, found := os.LookupEnv("ROUTER_PASSWORD")
 	if !found {
 		logger.Printf("ROUTER_PASSWORD not found")
 		return
 	}
-	routerController := controller.CreateRouterController(URL, Username, Password, kube)
+
+	hostFormat, found := os.LookupEnv("ROUTER_SERVICE_HOST_FORMAT")
+	if !found {
+		logger.Printf("ROUTER_SERVICE_HOST_FORMAT not found")
+		return
+	}
+
+	listenToIngresses := true
+	listenToLoadBalancers := true
+
+	ignoredIngress, found := os.LookupEnv("ROUTER_SERVICE_NO_INGRESSES")
+	if ignoredIngress == "true" || ignoredIngress == "1" {
+		listenToIngresses = false
+	}
+	ignoredLoadBalancers, found := os.LookupEnv("ROUTER_SERVICE_NO_LOADBALANCERS")
+	if ignoredLoadBalancers == "true" || ignoredLoadBalancers == "1" {
+		listenToLoadBalancers = false
+	}
+
+	routerController := controller.CreateRouterController(URL, username, password, hostFormat, kube, listenToIngresses, listenToLoadBalancers)
 	go func() {
 		// Watch for sigint
 		sigint := make(chan os.Signal, 1)
@@ -45,7 +81,7 @@ func main() {
 		<-sigint
 		routerController.Stop()
 	}()
-	err := routerController.Run(context.Background())
+	err = routerController.Run(context.Background())
 	if err != nil {
 		logger.Fatalf("Error running controller: %s", err.Error())
 		return
--- pkg/controller.go
+++ pkg/controller.go
@@ -3,34 +3,49 @@ package controller
 import (
 	"context"
 	"github.com/chickenzord/go-huawei-client/pkg/eg8145v5"
-	v1 "k8s.io/api/networking/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"log"
 	"reflect"
 	"slices"
+	"strings"
+	"sync"
 )
 
 type RouterIngressController struct {
-	logger           *log.Logger
-	routerClient     *eg8145v5.Client
-	kubernetesClient kubernetes.Interface
-	currentWatcher   watch.Interface
+	logger                 *log.Logger
+	routerClient           *eg8145v5.Client
+	kubernetesClient       kubernetes.Interface
+	ingressWatcher         watch.Interface
+	loadBalancerWatcher    watch.Interface
+	loadBalancerHostFormat string
+	lastIps                map[string]string
+	listenToIngresses      bool
+	listenToLoadBalancers  bool
 }
 type Clientset struct {
-	ingress *v1.Ingress
+	ingress *networkingv1.Ingress
 }
 
 func CreateRouterController(
 	URL string,
 	Username string,
 	Password string,
+	LoadBalancerHostFormat string,
 	KubernetesClient kubernetes.Interface,
+	ListenToIngresses bool,
+	ListenToLoadBalancers bool,
 ) RouterIngressController {
 	return RouterIngressController{
-		logger:           log.Default(),
-		kubernetesClient: KubernetesClient,
+		logger:                 log.Default(),
+		kubernetesClient:       KubernetesClient,
+		loadBalancerHostFormat: LoadBalancerHostFormat,
+		lastIps:                map[string]string{},
+		listenToIngresses:      ListenToIngresses,
+		listenToLoadBalancers:  ListenToLoadBalancers,
 		routerClient: eg8145v5.NewClient(eg8145v5.Config{
 			URL:      URL,
 			Username: Username,
@@ -38,10 +53,13 @@ func CreateRouterController(
 		}),
 	}
 }
-func (c *RouterIngressController) loop(ctx context.Context, channel chan<- error) {
+
+func (c *RouterIngressController) ingressWatch(ctx context.Context, waitGroup *sync.WaitGroup, channel chan<- error) {
+	defer waitGroup.Done()
+
 	c.logger.Printf("Starting ingresses watch")
 	watcher, err := c.kubernetesClient.NetworkingV1().Ingresses("").Watch(ctx, metav1.ListOptions{})
-	c.currentWatcher = watcher
+	c.ingressWatcher = watcher
 	if err != nil {
 		c.logger.Printf("Failed to watch ingresses: %s", err.Error())
 		channel <- err
@@ -49,13 +67,26 @@ func (c *RouterIngressController) loop(ctx context.Context, channel chan<- error
 	}
 
 	for event := range watcher.ResultChan() {
-		ingress, ok := event.Object.(*v1.Ingress)
+		ingress, ok := event.Object.(*networkingv1.Ingress)
 		if !ok {
 			c.logger.Printf("Received event %s with not ingress object: %s", event.Type, reflect.TypeOf(event.Object).Name())
 			continue
 		}
 		c.logger.Printf("Received event %s for ingress %s", event.Type, ingress.Name)
-		err := c.syncIngress(ingress)
+
+		var hosts []eg8145v5.StaticDnsHost
+		for _, lbIngress := range ingress.Status.LoadBalancer.Ingress {
+
+			for _, rule := range ingress.Spec.Rules {
+
+				hosts = append(hosts, eg8145v5.StaticDnsHost{
+					DomainName: rule.Host,
+					IPAddress:  lbIngress.IP,
+				})
+			}
+		}
+
+		err := c.syncHosts(hosts)
 		if err != nil {
 			channel <- err
 			return
@@ -65,70 +96,156 @@ func (c *RouterIngressController) loop(ctx context.Context, channel chan<- error
 	channel <- nil
 }
 
+const nameLabel = "app.kubernetes.io/name"
+
+func selectServiceName(service *corev1.Service) string {
+	s := service.Labels[nameLabel]
+	if len(s) > 0 {
+		return s
+	}
+	return service.Name
+}
+func (c *RouterIngressController) loadBalancerWatch(ctx context.Context, waitGroup *sync.WaitGroup, channel chan error) {
+	defer waitGroup.Done()
+
+	c.logger.Printf("Starting loadBalancer watch")
+	watcher, err := c.kubernetesClient.CoreV1().Services("").Watch(ctx, metav1.ListOptions{})
+	c.ingressWatcher = watcher
+	if err != nil {
+		c.logger.Printf("Failed to watch ingresses: %s", err.Error())
+		channel <- err
+		return
+	}
+
+	for event := range watcher.ResultChan() {
+		service, ok := event.Object.(*corev1.Service)
+
+		if !ok {
+			c.logger.Printf("Received event %s with not service object: %s", event.Type, reflect.TypeOf(event.Object).Name())
+			continue
+		}
+
+		if service.Spec.Type != corev1.ServiceTypeLoadBalancer {
+			continue
+		}
+
+		name := selectServiceName(service)
+		host := strings.Replace(c.loadBalancerHostFormat, "{name}", name, -1)
+
+		var hosts []eg8145v5.StaticDnsHost
+
+		for _, lbIngress := range service.Status.LoadBalancer.Ingress {
+			previous := c.lastIps[host]
+			if len(previous) > 0 && previous == lbIngress.IP {
+				continue
+			}
+
+			hosts = append(hosts, eg8145v5.StaticDnsHost{
+				DomainName: host,
+				IPAddress:  lbIngress.IP,
+			})
+			c.lastIps[host] = lbIngress.IP
+		}
+		if len(hosts) > 0 {
+			c.logger.Printf("Reacting to event %s for service %s", event.Type, service.Name)
+			err := c.syncHosts(hosts)
+			if err != nil {
+				channel <- err
+				return
+			}
+		}
+	}
+	c.logger.Printf("Finished watching loadBalancers")
+	channel <- nil
+}
+
 func (c *RouterIngressController) Run(ctx context.Context) error {
-	channel := make(chan error)
-	go c.loop(ctx, channel)
+	group := sync.WaitGroup{}
+	jobCount := 0
+	if c.listenToIngresses {
+		jobCount++
+	}
+	if c.listenToLoadBalancers {
+		jobCount++
+	}
+	group.Add(jobCount)
+	channel := make(chan error, jobCount)
+
+	if c.listenToIngresses {
+		go c.ingressWatch(ctx, &group, channel)
+	}
+
+	if c.listenToLoadBalancers {
+		go c.loadBalancerWatch(ctx, &group, channel)
+	}
 	err := c.routerClient.Login()
 	if err != nil {
 		return err
 	}
 
+	c.logger.Printf("Controller started.")
+	group.Wait()
+	c.logger.Printf("Controller finishing execution.")
+	c.logger.Printf("Checking for errors...")
 	err = <-channel
 	if err != nil {
+		c.logger.Printf("Error: %s", err.Error())
 		return err
 	}
+	c.logger.Printf("Controller finished execution.")
 	return nil
 }
 
 func (c *RouterIngressController) Stop() {
 	c.logger.Print("Stopping controller")
-	if c.currentWatcher != nil {
-		c.currentWatcher.Stop()
+	if c.ingressWatcher != nil {
+		c.ingressWatcher.Stop()
+	}
+	if c.loadBalancerWatcher != nil {
+		c.loadBalancerWatcher.Stop()
 	}
 }
 
-func (c *RouterIngressController) syncIngress(ingress *v1.Ingress) error {
-	hosts, err := c.routerClient.GetAllStaticDnsHosts()
+func (c *RouterIngressController) syncHosts(hosts []eg8145v5.StaticDnsHost) error {
+	if len(hosts) == 0 {
+		return nil
+	}
+
+	existingHosts, err := c.routerClient.GetAllStaticDnsHosts()
 	if err != nil {
 		err := c.routerClient.Login()
 		if err != nil {
 			return err
 		}
-		hosts, err = c.routerClient.GetAllStaticDnsHosts()
+		existingHosts, err = c.routerClient.GetAllStaticDnsHosts()
 		if err != nil {
 			return err
 		}
 	}
-	for _, status := range ingress.Status.LoadBalancer.Ingress {
-		for _, rule := range ingress.Spec.Rules {
 
-			host := eg8145v5.StaticDnsHost{
-				DomainName: rule.Host,
-				IPAddress:  status.IP,
+	for _, host := range hosts {
+		dnsEntryIndex := slices.IndexFunc(existingHosts, func(other eg8145v5.StaticDnsHost) bool {
+			return host.DomainName == other.DomainName
+		})
+		exists := dnsEntryIndex != -1
+		if exists {
+			// Update
+			dnsHost := existingHosts[dnsEntryIndex]
+			if dnsHost.IPAddress == host.IPAddress {
+				c.logger.Printf("Host %s up to date with ip %s", host.DomainName, host.IPAddress)
+				continue
 			}
-
-			dnsEntryIndex := slices.IndexFunc(hosts, func(host eg8145v5.StaticDnsHost) bool {
-				return rule.Host == host.DomainName
-			})
-			exists := dnsEntryIndex != -1
-			if exists {
-				dnsHost := hosts[dnsEntryIndex]
-				if dnsHost.IPAddress == host.IPAddress {
-					c.logger.Printf("Host %s up to date with ip %s", host.DomainName, host.IPAddress)
-					continue
-				}
-				c.logger.Printf("Updating existing host %s on index %d to ip %s", host.DomainName, dnsEntryIndex, host.IPAddress)
-				err := c.routerClient.SetDnsHost(host, dnsEntryIndex)
-				if err != nil {
-					return err
-				}
-			} else {
-				// Create
-				c.logger.Printf("Creating new host %s with ip %s", host.DomainName, host.IPAddress)
-				err := c.routerClient.AddDnsHost(host)
-				if err != nil {
-					return err
-				}
+			c.logger.Printf("Updating existing host %s on index %d to ip %s", host.DomainName, dnsEntryIndex, host.IPAddress)
+			err := c.routerClient.SetDnsHost(host, dnsEntryIndex)
+			if err != nil {
+				return err
+			}
+		} else {
+			// Create
+			c.logger.Printf("Creating new host %s with ip %s", host.DomainName, host.IPAddress)
+			err := c.routerClient.AddDnsHost(host)
+			if err != nil {
+				return err
 			}
 		}
 	}
